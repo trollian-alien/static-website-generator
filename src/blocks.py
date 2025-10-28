c6bd6b6027c22be957d956c66f0889d6765c7dc1
trollian-alien <epciscientician@gmail.com>
2025-10-28 11:12:01 +0300

    markdown splitter to its blocks

---
+++ src/blocks.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    return [block.strip() for block in blocks if block != ""]
+
+def block_to_block_type(block):
+    if block[0] == "#"and block[6] != "#":
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    lines = block.split("\n")
+    if False not in [line.startswith(">") for line in lines]:
+        return BlockType.QUOTE
+    elif False not in [line.startswith("-") for line in block.split("\n")]:
+        return BlockType.UNORDERED_LIST
+    elif False not in [lines[i].startswith(f"{i+1}.") for i in range(0,len(lines))]:
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
