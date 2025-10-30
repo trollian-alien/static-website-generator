a7c33778753135f5201502958918c42808450881
trollian-alien <epciscientician@gmail.com>
2025-10-30 11:15:47 +0300

    added copy function from one directory to another

--- src/main.py
+++ src/main.py
@@ -1,6 +1,30 @@
-from textnode import TextType, TextNode
+import os, shutil
+
+def wipe_directory_content(path):
+    with os.scandir(path) as entries:
+        for entry in entries:
+            if entry.is_dir() and not entry.is_symlink():
+                shutil.rmtree(entry.path)
+            else:
+                os.remove(entry.path)
+
+def copy(source, target): #copies from source directory path to target path
+    wipe_directory_content(target)
+    changelog = f"{target} content wiped"
+    with os.scandir(source) as entries:
+        for entry in entries:
+            if entry.is_dir() and not entry.is_symlink():
+                newdir = f"{target}/{entry.name}"
+                os.mkdir(newdir)
+                copy(entry, newdir)
+                changelog += f"\n directory {entry.name} added to {target}"
+            else:
+                shutil.copy(entry, target)
+                changelog += f"\n file {entry.name} copied to {target}"
+    print(changelog)
 
 def main():
-    return TextNode("pizza", TextType.BOLD)
+    copy(os.path.abspath("static"), os.path.abspath("public"))
 
-print(main())
+if __name__ == "__main__":
+    main()
